9c03d72ac8751022edea7498e74bdf246d2c9e1d
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-02-24 15:20:41 +0100

    Changed declaration of function, redundant code deleted

--- src/rpn/rpn.test.ts
+++ src/rpn/rpn.test.ts
@@ -5,16 +5,12 @@ describe("rpn()", () => {
     expect(rpn("2")).toBe(2);
   });
 
-  it("correctly adds 2 numbers", function () {
+  it("correctly adds 2 numbers", () => {
     expect(rpn("2 0 +")).toBe(2);
   });
 
-  it.only("returns 4 when given '1 3 +'", () => {
+  it("returns 4 when given '1 3 +'", () => {
     expect(rpn("1 3 +")).toBe(4);
-  });
-
-  it(`correctly adds 2 numbers`, async () => {
-    expect(rpn("2 2 +")).toBe(4);
   });
 
   it("correctly divides  2  numbers", () => {

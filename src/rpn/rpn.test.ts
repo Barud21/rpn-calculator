8c3fc3157c51ed8b9fe0c0417891dddb8c222d3c
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-02-24 16:20:20 +0100

    Test returns 12 when given '2 2 + 3 *'

--- src/rpn/rpn.test.ts
+++ src/rpn/rpn.test.ts
@@ -13,6 +13,10 @@ describe("rpn()", () => {
     expect(rpn("1 3 +")).toBe(4);
   });
 
+  it("returns 12 when given '2 2 + 3 *'", () => {
+    expect(rpn("2 2 + 3 *")).toBe(12);
+  });
+
   it("correctly divides  2  numbers", () => {
     expect(rpn("2 2 /")).toBe(1);
   });
